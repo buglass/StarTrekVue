51556168e1e1ba946277978fc0380d853b4d6ce9
buglass <38510232+buglass@users.noreply.github.com>
2025-09-04 21:49:30 +0100

    display list of integers in galaxy

--- src/views/GalaxyView.vue
+++ src/views/GalaxyView.vue
@@ -1,5 +1,14 @@
 <template>
   <div>Welcome to the galaxy</div>
+  <div v-for="quadrant in quadrants">
+    <div>Quadrant {{ quadrant }}</div>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const quadrants = ref([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+</script>
 
 <style></style>
