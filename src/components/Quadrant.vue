8d884bdaef0366e251c89e38fe80ee2af422b2d1
buglass <38510232+buglass@users.noreply.github.com>
2025-09-04 22:07:12 +0100

    fill galaxy with 10x10 quadrants

---
+++ src/components/Quadrant.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid grid-cols-10 grid-rows-10">
+    <div v-for="tile in tiles">
+      <div>{{ tile }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const tiles = ref(new Array<string>(100).fill('*'))
+</script>
+
+<style></style>
--- src/views/GalaxyView.vue
+++ src/views/GalaxyView.vue
@@ -1,16 +1,17 @@
 <template>
   <div>Welcome to the galaxy</div>
-  <div class="grid grid-cols-10 grid-rows-10">
+  <div class="grid grid-cols-10 grid-rows-10 gap-10">
     <div v-for="quadrant in quadrants">
-      <div>{{ quadrant }}</div>
+      <Quadrant />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import Quadrant from '@/components/Quadrant.vue'
 import { ref } from 'vue'
 
-const quadrants = ref(new Array<string>(100).fill('*'))
+const quadrants = ref(new Array(100))
 </script>
 
 <style></style>
