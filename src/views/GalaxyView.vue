eb4a96c2556f6281aac06b9713bd8df90b346a5d
buglass <38510232+buglass@users.noreply.github.com>
2025-09-04 22:08:54 +0100

    add a TODO to the galaxy View

--- src/views/GalaxyView.vue
+++ src/views/GalaxyView.vue
@@ -10,6 +10,10 @@
 <script setup lang="ts">
 import Quadrant from '@/components/Quadrant.vue'
 import { ref } from 'vue'
+// TODO - In the game each quadrant was shown by a X-Y-Z
+// configuration which was Klingons-Stars-Something.
+//
+// Only your current quadrant showed the whole grid, IIRC.
 
 const quadrants = ref(new Array(100))
 </script>
